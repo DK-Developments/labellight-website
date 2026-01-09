910a173290618aa75eaaecbf362665b23bec5015
david <jackbunckenburg@gmail.com>
2026-01-09 15:45:03 +1300

    update config.js - local version

--- src/website/config.js
+++ src/website/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   COGNITO_DOMAIN: 'printerapp-auth-dev.auth.ap-southeast-2.amazoncognito.com',
   CLIENT_ID: '4im3pc7sdlbnc1q6p7odslmmc1',
-  API_URL: '[https://lhsb9ev8t3.execute-api.ap-southeast-2.amazonaws.com/dev](https://lhsb9ev8t3.execute-api.ap-southeast-2.amazonaws.com/dev)',
+  API_URL: 'https://lhsb9ev8t3.execute-api.ap-southeast-2.amazonaws.com/dev',
 };
